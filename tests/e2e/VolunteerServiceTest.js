ea0153d3127ef8b9aac6d03bfb3916b37791a325
Radina Antonova <rantonova@aixigo.de>
2019-08-14 08:46:06 +0200

    pauses removed

--- tests/e2e/VolunteerServiceTest.js
+++ tests/e2e/VolunteerServiceTest.js
@@ -23,8 +23,6 @@ module.exports = { // adapted from: https://git.io/vodU0
          .waitForElementVisible( '#pickButton', 1000 )
          .waitForElementVisible( '#loadButton2', 1000 )
          .waitForElementVisible( '#myLastReviewersTable', 1000 )
-         //.waitForElementVisible('#volunteerName', 1000)
-         //.click('#b1')
          .saveScreenshot( path.join( config.SCREENSHOT_PATH, 'BodyVisibility.png' ) )
          .end();
 
@@ -47,7 +45,6 @@ module.exports = { // adapted from: https://git.io/vodU0
          .end();
    },
 
-   //always deletes the first element in the table because the ids are not unique
    'Step 3: delete user'( browser ) {
       browser
          .url( 'http://localhost:3000/' )
@@ -69,16 +66,12 @@ module.exports = { // adapted from: https://git.io/vodU0
          .waitForElementVisible( 'body', 1000 )
          .waitForElementVisible( '#loadButton1', 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
          .waitForElementVisible( '#toggleButton' )
          .click( '#toggleButton' )
-         .pause( 1000 )
          .acceptAlert()
-         .pause( 1000 )
          .click( '#toggleButton' )
          .pause( 1000 )
          .acceptAlert()
-         .pause( 1000 )
          .saveScreenshot( path.join( config.SCREENSHOT_PATH, 'ToggleUser.png' ) )
          .end();
    },
@@ -89,18 +82,14 @@ module.exports = { // adapted from: https://git.io/vodU0
          .waitForElementVisible( 'body', 1000 )
          .waitForElementVisible( '#loadButton1', 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
          .waitForElementVisible( '#submitButtonVolunteer', 1000 )
          .refresh()
          .setValue( '#volunteerName', 'FrontTest' )
          .click( '#f1' )
-         .pause( 1000 )
          .click( '#submitButtonVolunteer' )
          .pause( 1000 )
          .acceptAlert()
-         .pause( 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
          .assert.containsText( '#frontEndCell', 'FrontTest' )
          .saveScreenshot( path.join( config.SCREENSHOT_PATH, 'AddFrontendUser.png' ) )
          .end();
@@ -112,18 +101,14 @@ module.exports = { // adapted from: https://git.io/vodU0
          .waitForElementVisible( 'body', 1000 )
          .waitForElementVisible( '#loadButton1', 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
          .waitForElementVisible( '#submitButtonVolunteer', 1000 )
          .refresh()
          .setValue( '#volunteerName', 'BackTest' )
          .click( '#b1' )
-         .pause( 1000 )
          .click( '#submitButtonVolunteer' )
          .pause( 1000 )
          .acceptAlert()
-         .pause( 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
          .assert.containsText( '#backEndCell', 'BackTest' )
          .saveScreenshot( path.join( config.SCREENSHOT_PATH, 'AddBackendUser.png' ) )
          .end();
@@ -135,80 +120,18 @@ module.exports = { // adapted from: https://git.io/vodU0
          .waitForElementVisible( 'body', 1000 )
          .waitForElementVisible( '#loadButton1', 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
          .waitForElementVisible( '#submitButtonVolunteer', 1000 )
          .refresh()
          .setValue( '#volunteerName', 'FrontBackTest' )
          .click( '#b1' )
          .click( '#f1' )
-         .pause( 1000 )
          .click( '#submitButtonVolunteer' )
          .pause( 1000 )
          .acceptAlert()
-         .pause( 1000 )
          .click( '#loadButton1' )
-         .pause( 1000 )
-         //.assert.containsText('#frontEndCell', 'FrontBackTest')
          .assert.assertText( '#frontEndCell', 'FrontBackTest' )
-      // .assert.containsText('#backEndCell', 'FrontBackTest') //ids are not unique -> test fails
          .saveScreenshot( path.join( config.SCREENSHOT_PATH, 'AddBackendUser.png' ) )
          .end();
    }
-   /*
-      'Step 6: submit a backend volunteer': function(browser) {
-         browser
-            .url('http://localhost:3000/')
-            .waitForElementVisible('body', 1000)
-            .waitForElementVisible('#loadButton1',1000)
-            .click('#loadButton1')
-            .pause(1000)
-            .waitForElementVisible('#submitButtonBack',1000)
-            .refresh()
-            .setValue('#backendName','BackTest')
-            .pause(1000)
-            .click('#submitButtonBack')
-            .pause(1000)
-            .acceptAlert()
-            .pause(1000)
-            .click('#loadButton1')
-            .pause(1000)
-            .assert.containsText( '#myTable', 'BackTest' )
-            .saveScreenshot(path.join(config.SCREENSHOT_PATH, 'AddBackendUser.png'))
-            .end();
-      }
-   /*
-
-   //Test works, have to fix bug
-      'Step 6: check if a volunteer can be frontend and backend at the same time': function(browser) {
-         browser
-            .url('http://localhost:3000/')
-            .waitForElementVisible('body', 1000)
-            .waitForElementVisible('#loadButton1',1000)
-            .click('#loadButton1')
-            .pause(1000)
-            .waitForElementVisible('#submitButtonBack',1000)
-            .waitForElementVisible('#submitButtonFront', 1000)
-            .refresh()
-            .setValue('#backendName','FrontBackTest')
-            .pause(1000)
-            .click('#submitButtonBack')
-            .pause(1000)
-            .acceptAlert()
-            .pause(1000)
-            .setValue('#frontendName','FrontBackTest')
-            .pause(1000)
-            .click('#submitButtonFront')
-            .pause(1000)
-            .acceptAlert()
-            .pause(1000)
-            .click('#loadButton1')
-            .pause(1000)
-            .assert.containsText( '#frontEndCell', 'FrontBackTest' )
-            .assert.containsText('#backEndCell', 'FrontBackTest')
-            .saveScreenshot(path.join(config.SCREENSHOT_PATH, 'AddBackendUser.png'))
-            .end();
-      }
-
-      */
 };
 
